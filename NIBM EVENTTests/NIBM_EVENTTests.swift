4df3483cd006284b15c620bbee09d55cef71ce63
gayanimadahapola <gayanimadahapola@users.github.com>
2020-02-04 00:40:29 +0530

    Initial Commit

---
+++ NIBM EVENTTests/NIBM_EVENTTests.swift
@@ -0,0 +1,34 @@
+//
+//  NIBM_EVENTTests.swift
+//  NIBM EVENTTests
+//
+//  Created by Gayani Madubhashini on 2/4/20.
+//  Copyright © 2020 Gayani Madubhashini. All rights reserved.
+//
+
+import XCTest
+@testable import NIBM_EVENT
+
+class NIBM_EVENTTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
